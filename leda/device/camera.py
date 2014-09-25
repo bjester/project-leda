a5c0de93aa31d71c5b64709fd6f6131cafed87fe
jeff <jeff@LittleBlue.(none)>
2014-09-25 16:55:55 -0700

    camera "Hello, World" added

--- leda/device/camera.py
+++ leda/device/camera.py
@@ -1,4 +1,16 @@
-from leda.device import device
+from time import sleep
+import picamera
 
-class Camera(device.Device):
-    pass
+WIDTH=1280
+HEIGHT=1024
+
+
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    camera.capture('~/image.jpg', format='jpg', resize=(WIDTH,HEIGHT))
+    camera.stop_preview()
+
+
+#from leda.device import device
+#class Camera(device.Device):   # Camera inherits from device.Device object??
+#    pass
