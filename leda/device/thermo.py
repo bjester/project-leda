629286b4c9ce706463c98b9cfcf89607a1de394b
mr1337357 <mr1337357@gmail.com>
2014-11-08 16:55:36 -0800

    added thermo driver

--- leda/device/thermo.py
+++ leda/device/thermo.py
@@ -2,18 +2,26 @@
 "Converts Temperature-to-digital Word in 750ms (max)"
 
 from leda.device import device
+import smbus
 
 class Thermo(device.Device):
     """Project Ledas Thermostat object"""
-
+    bus = None
+    address = None
     # __init__(self, extraParams) acts like a constructor but this is a normal function
-    def init(self, time):
+    def init(self, time, address, bus):
         """Init resources and attach interval for recurring capture"""
-        pass
+        self.bus = bus
+        self.address = address
     
     def capture(self):
         """Capture temperature data"""
-        pass
+        temp = self.bus.read_word_data(self.address,0xAA)
+        temp = (temp/256+temp*256)%65536
+        if temp & 0x8000:
+            temp = temp - 0x10000
+        temp = temp / 256.0
+        return temp
 
     def end(self):
         """If necessary, deallocate resources"""
