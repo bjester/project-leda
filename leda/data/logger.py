8ea0a8af2550db8eea0cfdc5d79358ec1c804178
Blaine Jester <blainesworld@gmail.com>
2015-04-18 15:38:45 -0700

    Convert logger to record binary

--- leda/data/logger.py
+++ leda/data/logger.py
@@ -5,36 +5,16 @@ class Logger():
 
     def __init__(self, fileName):
         """Constructor, set file name for logging"""
-        self.fileName = None
-        self.fileIndex = 0
-        self.bites = 0
-        self.limit = 1024*1024*1024
-        self.fileHandle = None
         self.fileName = fileName
-        #self.fileHandle = open(self.fileName, 'w')
-        #self.csvHandle = csv.writer(self.fileHandle)
+        self.fileHandle = None
 
     def begin(self, header):
         """Start file logging, add header to file"""
-        self.csvHandle.writerow(header)
-        pass
+        self.fileHandle = open(self.fileName, 'wb')
 
-    def record(self, row):
+    def append(self, data):
         """Record data row"""
-        print "recorded row:", row
-        # self.bytes += self.csvHandle.writerows(data)
-        with open(self.fileName, 'wb') as csvfile:
-            logwriter = csv.writer(csvfile, delimiter=' ', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
-            logwriter.writerows(row)
-
-        # How to get bytes added with each addition? Rotate files, can we use linux util?
-        if self.bites > self.limit:
-            self.end()
-            self.fileIndex += 1
-            self.fileName = "%s.%d" % (self.fileName, self.fileIndex)
-            self.start()
-        # Dump entries to file as often as possible
-        #self.fileHandle.flush()
+        self.fileHandle.write(data)
 
     def end(self):
         """End logging"""
