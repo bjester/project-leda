e60c7120b9fc33c6e3d7fd381ebfbf9cb79d675b
mr1337357 <mr1337357@gmail.com>
2015-04-18 20:23:13 -0700

    adding manual gpsd start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from leda import leda
+import os
 
 
 
@@ -19,11 +20,13 @@ serial_timeout = 1               # max time to wait for serial response
 
 
 #########################################################
-t = leda.Leda(cam_period, 
+if __name__ = "__main__":
+    os.system("gpsd /dev/ttyUSB0 -F /var/run/gpsd.pid")
+    t = leda.Leda(cam_period, 
               serial_period,
               serial_path,
               baud,
               serial_timeout)
 
-t.start()
+    t.start()
 
