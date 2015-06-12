f88d8729b9fa38275726a7029252f6f5c8a70c91
jeffBunce <bunce32@gmail.com>
2015-06-12 06:03:48 +0000

    sync up uart objcet to daughter board and test

--- leda/device/uart.py
+++ leda/device/uart.py
@@ -4,35 +4,49 @@ import time
 import io
 
 class Uart:
+    RECEIVED_BYTES = 26
 
     def __init__(self, device_path, baud, tout):
         """Init resources and attach interval for recurring commands"""
         self.device = device_path
-        self.ser = serial.Serial(device_path, baudrate=baud, timeout=tout)
-        self.ser.flushInput()
+        toutSeconds = tout/1000
+        self.ser = serial.Serial(device_path, baudrate=baud, timeout=toutSeconds)
+        #self.ser.flushInput()
 
 
+    #1. timeout -> set up by python automatically
+    #2. \n can be encountered in middle of packet (unlikely) but byte for byte probably safer
     def capture(self):
         """Capture all daughter board sensor data"""
         # request 'S\n'
         self.ser.write("S\n")
         # wait for Ack that data is ready  (receive 'A\n')
         ack = self.ser.readline();
-        if ack == "A\n":
-            return false # received bad data
+        if ack != "Z\n":
+            return False # received bad data
         print("Ack received")
+        #print str(what)
         # request the data
         self.ser.write("R\n")
         #time.sleep(1)
-        # read the data
-        return self.ser.readline() #need to remove line break
+        # read the data; 14 bytes
+        result = []
+        for x in range(0, RECEIVED_BYTES):
+            result += self.ser.readline() 
+        return str(result)
 
-    def end(self):
+    def close(self):
         """If necessary, deallocate resources"""
-        self.port.close()
+        self.ser.close()
 
 
 #TEST AREA
 uart = Uart("/dev/ttyACM0", 38400, 1000)
-print uart.capture()
+cap = uart.capture()
+print("Received bad data" if cap == False else cap)
+cap = uart.capture()
+print("Received bad data" if cap == False else cap)
+cap = uart.capture()
+print("Received bad data" if cap == False else cap)
+uart.close()
 
