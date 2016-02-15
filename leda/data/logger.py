c95366e98797e387e52b9e30954ad8f6a92827c6
jeffBunce <bunce32@gmail.com>
2016-02-14 21:27:38 -0800

    switched to basic CSV, not python CSV

--- leda/data/logger.py
+++ leda/data/logger.py
@@ -1,8 +1,3 @@
-#So as to not waste tremendous amounts of memory, do we
-#  need to write our data in 512kb blocks because of
-#  SD card????
-
-
 from datetime import datetime
 import time
 import csv
@@ -14,59 +9,24 @@ class Logger():
         """Constructor, set file name for logging"""
         self.path = path
         self.currentLog = None
-        self.openFile = None #required by close()
-        self.fileHandle = None #csv file
+        self.openFile = None 
 
     def open(self):
         """Start file logging, add header to file"""
         self.currentLog = self.path + "/rawdata.csv"
         self.openFile = open(self.currentLog, 'w', 1) #line buffered file
-        self.fileHandle = csv.writer(self.openFile)
 
     def append(self, data, timeStamp):
         """Record data row"""
-        if self.fileHandle is not None:
-            row = [[datetime.fromtimestamp(timeStamp).strftime('%Y-%m-%d %H:%M:%S')],
-                   [data]]
-            self.fileHandle.writerow(row)
+        if self.openFile is not None:
+            row = datetime.fromtimestamp(timeStamp).strftime('%Y-%m-%d %H:%M:%S') + "," + data
+            self.openFile.write(row)
         else:
             print("Must call Logger.open() before Logger.append()")
 
     def close(self):
         """End logging"""
-        if self.fileHandle is not None:
+        if self.openFile is not None:
             self.openFile.close()
-            self.fileHandle = None
             self.openFile = None
-
-    def decodeLastLog(self):
-        """Demonstration of (basic) decoding of log file"""
-        if self.currentLog is not None:
-            f = open(self.currentLog, 'r')
-            try:
-                reader = csv.reader(f)
-                lineCount = 1
-                for row in reader:
-                    line = ""
-                    try:
-                        flt = float(row[0][1:-1])
-                        stamp = datetime.fromtimestamp(flt)
-                    except ValueError:
-                        stamp = "Invalid timestamp"
-                    line += str(stamp)
-                    line += "     "
-                    line += row[1][1:-1]
-                    print(line)
-            finally:
-                f.close()
-
-
-	
-#log = Logger("test")
-#log.openLog()
-#log.append(45, datetime.now())
-#log.append(872, datetime.now())
-#log.append(6, datetime.now())
-#log.closeLog()
-#log.decodeLastLog()
 
